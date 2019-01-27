3a253e1ce6a97fe83332fe037ad13ce53c4fe7c0
Dirk Dobson <dirkvancouver@live.com>
2019-01-26 18:54:17 -0700

    global error handling

--- Program.cs
+++ Program.cs
@@ -4,9 +4,30 @@ namespace Methods
 {
     class Program
     {
+        public class Calculator
+        {
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(new Point(40, 60));
+                System.Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                System.Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y); 
+            }
+            catch (Exception)
+            {
+                System.Console.WriteLine("An unexpected error occured");
+            }
+           
         }
     }
 }
